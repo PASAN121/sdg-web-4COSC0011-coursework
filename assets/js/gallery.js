b5e17e7833faac7ca81a90e80a8e4c71e1e8fa51
Pasanchandima123 <PASANCHANDI@GMAIL.COM>
2026-03-23 11:58:08 +0530

    hover galler js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,7 +1,14 @@
 // JavaScript gallery file
 
-const selection=document.querySelectorAll(".gallery-section")
+const selection=document.querySelectorAll(".cell")
  
 selection.forEach(element => {
-    
+    element.addEventListener("mouseenter", function () {
+        this.style.borderLeft = "5px solid #ffffff";
+    });
+
+    element.addEventListener("mouseleave", function () {
+        this.style.borderLeft = "none";
+    });
 });
+
